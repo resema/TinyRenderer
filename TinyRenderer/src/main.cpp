ddd81afe6063639e2a619cbd374340babdebbe22
Renato Semadeni <renato.semadeni@gmail.com>
2018-01-10 17:43:10 +0100

    our_gl finished

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -17,44 +17,44 @@ Vec3f eye(1,1,3);
 Vec3f center(0,0,0);
 //Vec3f camera(0, 0, 3);
 
-Vec3f m2v(Matrix m) {
-	return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
-}
+//Vec3f m2v(Matrix m) {
+//	return Vec3f(m[0][0] / m[3][0], m[1][0] / m[3][0], m[2][0] / m[3][0]);
+//}
+//
+//Matrix v2m(Vec3f v) {
+//	Matrix m(4, 1);
+//	m[0][0] = v.x;
+//	m[1][0] = v.y;
+//	m[2][0] = v.z;
+//	m[3][0] = 1.f;
+//	return m;
+//}
+//
+//Matrix modelview(Vec3f eye, Vec3f center, Vec3f up) {
+//    Vec3f z = (eye-center).normalize();
+//    Vec3f x = (up^z).normalize();
+//    Vec3f y = (z^x).normalize();
+//    Matrix mv = Matrix::identity(4);
+//    for (int i=0; i<3; i++) {
+//        mv[0][i] = x[i];
+//        mv[1][i] = y[i];
+//        mv[2][i] = z[i];
+//        mv[i][3] = -center[i];
+//    }
+//    return mv;
+//}
 
-Matrix v2m(Vec3f v) {
-	Matrix m(4, 1);
-	m[0][0] = v.x;
-	m[1][0] = v.y;
-	m[2][0] = v.z;
-	m[3][0] = 1.f;
-	return m;
-}
-
-Matrix modelview(Vec3f eye, Vec3f center, Vec3f up) {
-    Vec3f z = (eye-center).normalize();
-    Vec3f x = (up^z).normalize();
-    Vec3f y = (z^x).normalize();
-    Matrix mv = Matrix::identity(4);
-    for (int i=0; i<3; i++) {
-        mv[0][i] = x[i];
-        mv[1][i] = y[i];
-        mv[2][i] = z[i];
-        mv[i][3] = -center[i];
-    }
-    return mv;
-}
-
-Matrix viewport(int x, int y, int w, int h) {
-	Matrix m = Matrix::identity(4);
-	m[0][3] = x + w / 2.f;
-	m[1][3] = y + h / 2.f;
-	m[2][3] = depth / 2.f;
-
-	m[0][0] = w / 2.f;
-	m[1][1] = h / 2.f;
-	m[2][2] = depth / 2.f;
-	return m;
-}
+//Matrix viewport(int x, int y, int w, int h) {
+//	Matrix m = Matrix::identity(4);
+//	m[0][3] = x + w / 2.f;
+//	m[1][3] = y + h / 2.f;
+//	m[2][3] = depth / 2.f;
+//
+//	m[0][0] = w / 2.f;
+//	m[1][1] = h / 2.f;
+//	m[2][2] = depth / 2.f;
+//	return m;
+//}
 
 void triangle(Vec3i t0, Vec3i t1, Vec3i t2, Vec2i uv0, Vec2i uv1, Vec2i uv2, float ity0, float ity1, float ity2, TGAImage &image, int *zbuffer) {
 	if (t0.y == t1.y && t0.y == t2.y) return; // i dont care about degenerate triangles

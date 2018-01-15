907964628ef8ad73f364c37ad35fe0059cef59a0
Renato Semadeni <renato.semadeni@gmail.com>
2018-01-15 06:40:16 +0100

    started refactoring to starting point

--- TinyRenderer/src/our_gl.cpp
+++ TinyRenderer/src/our_gl.cpp
@@ -15,11 +15,10 @@ void viewport(int x, int y, int w, int h)
 	Viewport = Matrix::identity();
 	Viewport[0][3] = x + w / 2.f;
 	Viewport[1][3] = y + h / 2.f;
-	Viewport[2][3] = 255.f / 2.f;
-
+	Viewport[2][3] = 1.f;
 	Viewport[0][0] = w / 2.f;
 	Viewport[1][1] = h / 2.f;
-	Viewport[2][2] = 255.f / 2.f;
+	Viewport[2][2] = 0;
 }
 
 void projection(float coeff)

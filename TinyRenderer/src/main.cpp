8856ef729b941df22a46c7616f362a0327f5c149
resema <5762933+resema@users.noreply.github.com>
2018-01-16 21:53:51 +0100

    Normalmap in Darboux space

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -17,9 +17,14 @@ Vec3f center{ 0,0,0 };
 Vec3f up{ 0,1,0 };
 
 struct Shader : public IShader {
-	mat<2, 3, float> varying_uv;	// same as above 
-	mat<3, 3, float> varying_nrm;	// normal per vertex to be interpolated by FS
+	// triangle uv coordinates, written by the vertex shader, read by the fragment shader
+	mat<2, 3, float> varying_uv;
+	// triangle coordinates (clip coordinates), written by VS, read by FS
 	mat<4, 3, float> varying_tri;
+	// normal per vertex to be interpolated by FS
+	mat<3, 3, float> varying_nrm;
+	// triangle in normalized device coordinates
+	mat<3, 3, float> varying_ndc_tri;
 
 	virtual Vec4f vertex(int iface, int nthvert) {
 		// set 2x3 matrix with vertices idx (0..2) and uv-values (Vec2f)
@@ -29,6 +34,8 @@ struct Shader : public IShader {
 		// read the vertex from .obj file
 		Vec4f gl_Vertex = Projection * ModelView * embed<4>(model->vert(iface, nthvert));
 		varying_tri.set_col(nthvert, gl_Vertex);
+		// create the normal device coordinates
+		varying_ndc_tri.set_col(nthvert, proj<3>(gl_Vertex / gl_Vertex[3]));
 		// return vertex coordinates
 		return gl_Vertex;
 	}
@@ -37,7 +44,25 @@ struct Shader : public IShader {
 		Vec3f bn = (varying_nrm * bar).normalize();
 		Vec2f uv = varying_uv * bar;
 
-		float diff = std::max(0.f, bn * light_dir);
+		mat<3, 3, float> A;
+		A[0] = varying_ndc_tri.col(1) - varying_ndc_tri.col(0);
+		A[1] = varying_ndc_tri.col(2) - varying_ndc_tri.col(0);
+		A[2] = bn;
+
+		mat<3, 3, float> AI = A.invert();
+
+		Vec3f i = AI * Vec3f(varying_uv[0][1] - varying_uv[0][0], varying_uv[0][2] - varying_uv[0][0], 0);
+		Vec3f j = AI * Vec3f(varying_uv[1][1] - varying_uv[1][0], varying_uv[1][2] - varying_uv[1][0], 0);
+
+		mat<3, 3, float> B;
+		B.set_col(0, i.normalize());
+		B.set_col(1, j.normalize());
+		B.set_col(2, bn);
+
+		Vec3f n = (B * model->normal(uv)).normalize();
+
+		// float diff = std::max(0.f, bn * light_dir);
+		float diff = std::max(0.f, n * light_dir);
 		color = model->diffuse(uv) * diff;
 
 		// no, we do not discard this pixel
@@ -76,5 +101,7 @@ int main(int argc, char** argv) {
 
 	frame.flip_vertically();
 	frame.write_tga_file("framebuffer.tga");
+
+	delete [] zbuffer;
 	return 0;
 }
